1a4ae49add7f8b082772c285c1a8f9140d86860a
raj <panchalraj36@gmail.com>
2024-09-19 21:20:30 +0530

    exp and projects

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,13 +1,14 @@
-import hero from "./hero.jpg";
-import meta from "./meta.png";
-import shopify from "./shopify.png";
-import starbucks from "./starbucks.png";
-import tesla from "./tesla.png";
-import logo from "./logo.svg";
-import fondesk from "./fondesk.png";
-import cybercom from "./cybercom.png";
-import mayash from "./mayashsvg.svg";
-
+import hero from "./hero.jpg"
+import meta from "./meta.png"
+import shopify from "./shopify.png"
+import starbucks from "./starbucks.png"
+import tesla from "./tesla.png"
+import logo from "./logo.svg"
+import fondesk from "./fondesk.png"
+import cybercom from "./cybercom.png"
+import mayash from "./mayashsvg.svg"
+import span from "./span-logo.png"
+import food from "./food.png"
 export {
   hero,
   meta,
@@ -18,4 +19,6 @@ export {
   cybercom,
   fondesk,
   mayash,
-};
+  span,
+  food,
+}
